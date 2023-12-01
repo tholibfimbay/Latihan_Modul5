e3d58213f6845681f6d7ced38494dc014b9cfbf9
tholibfimbay <127409399+tholibfimbay@users.noreply.github.com>
2023-12-01 09:37:36 +0700

    Add files via upload

---
+++ app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig{
+    public $host ="localhost";
+    public $user = "root";
+    public $password=  "";
+    public $database_name="latihanmodul5";
+    public $port= 3306;
+}
